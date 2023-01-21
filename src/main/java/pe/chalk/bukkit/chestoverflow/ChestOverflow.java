2902e3b2beb5d527684ce828069a2b4728f46bfb
Chalk <9481405+ChalkPE@users.noreply.github.com>
2023-01-21 14:23:12 +0900

    refactor: old code

--- src/main/java/pe/chalk/bukkit/chestoverflow/ChestOverflow.java
+++ src/main/java/pe/chalk/bukkit/chestoverflow/ChestOverflow.java
@@ -17,10 +17,9 @@
 
 package pe.chalk.bukkit.chestoverflow;
 
-import org.bukkit.Material;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -35,6 +34,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -54,54 +54,35 @@ public class ChestOverflow extends JavaPlugin implements Listener, CommandExecut
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerInteract(PlayerInteractEvent event){
+    public void onPlayerInteract(PlayerInteractEvent event) {
         final Player player = event.getPlayer();
-        if(!player.hasPermission("chestoverflow.use")) return;
-
-        if(event.hasItem()) return;
-        if(event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        if (!player.hasPermission("chestoverflow.use")) return;
+        if (event.hasItem() || event.getAction() != Action.LEFT_CLICK_BLOCK) return;
 
         final Block block = event.getClickedBlock();
-        if(block != null && ChestOverflow.handleChest(block, player)){
-            player.playSound(block.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 0.2f, 0.5f);
-        }
+        if (block != null && handleChest(block, player)) playSound(player, block.getLocation());
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) return true;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!command.getName().equals("sort")) return true;
-
-        final Player player = (Player) sender;
-        if (ChestOverflow.handlePlayer(player)) {
-            player.playSound(player.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 0.2f, 0.5f);
-        }
+        if (!(sender instanceof Player player)) return true;
+        if (handlePlayer(player)) playSound(player, player.getLocation());
         return true;
     }
 
-    public static boolean hasNormalInventory(final Block block){
-        final BlockState state = block.getState();
-        if(!(state instanceof InventoryHolder)) return false;
-
-        final Inventory inventory = ((InventoryHolder) state).getInventory();
-        return !(inventory instanceof FurnaceInventory || inventory instanceof BrewerInventory || inventory instanceof BeaconInventory);
+    public static void playSound(final Player player, final Location location) {
+        player.playSound(location, Sound.ENTITY_ARROW_HIT_PLAYER, 0.2f, 0.5f);
     }
 
-    public static Inventory getInventoryFromBlock(final Block block, final Player player){
-        if(block == null) return null;
-        if(block.getType() == Material.ENDER_CHEST && player != null) return player.getEnderChest();
-        if(block.getState() instanceof InventoryHolder && ChestOverflow.hasNormalInventory(block)) return ((InventoryHolder) block.getState()).getInventory();
-        return null;
-    }
-
-    public static boolean handleChest(final Block block, final Player player){
-        final Inventory inventory = getInventoryFromBlock(block, player);
-        if(inventory == null || inventory.getSize() <= 0 || inventory.getContents().length == 0) return false;
+    public static boolean handleChest(final Block block, final Player player) {
+        final Inventory inventory = ItemHelper.getInventoryFromBlock(block, player);
+        if (inventory == null || inventory.getSize() <= 0 || inventory.getContents().length == 0) return false;
 
         final List<ItemStack> stacks = Arrays.stream(inventory.getContents()).collect(Collectors.toList());
         final List<ItemStack> cleanStacks = ChestOverflow.sortedItemStacks(ChestOverflow.distinctItemStacks(stacks));
-        while(cleanStacks.size() > inventory.getMaxStackSize()) block.getWorld().dropItemNaturally(block.getLocation(), cleanStacks.remove(cleanStacks.size() - 1));
 
+        ItemHelper.dropItems(cleanStacks, inventory.getMaxStackSize(), block.getWorld(), block.getLocation());
         inventory.setContents(cleanStacks.toArray(ItemStack[]::new));
         block.getState().update(true);
         return true;
@@ -109,40 +90,39 @@ public class ChestOverflow extends JavaPlugin implements Listener, CommandExecut
 
     public static boolean handlePlayer(final Player player) {
         final Inventory inventory = player.getInventory();
-        final List<ItemStack> stacks = Arrays.stream(inventory.getStorageContents()).collect(Collectors.toList());
+        if (inventory.getSize() <= 0 || inventory.getStorageContents().length == 0) return false;
 
+        final List<ItemStack> stacks = Arrays.stream(inventory.getStorageContents()).collect(Collectors.toList());
         final List<ItemStack> hotbarStacks = stacks.subList(0, 9);
         final List<ItemStack> storageStacks = stacks.subList(9, stacks.size());
         final List<ItemStack> sortedStacks = ChestOverflow.sortedItemStacks(ChestOverflow.distinctItemStacks((storageStacks)));
         final List<ItemStack> cleanStacks = Stream.concat(hotbarStacks.stream(), sortedStacks.stream()).collect(Collectors.toList());
 
-        while(cleanStacks.size() > inventory.getMaxStackSize()) player.getWorld().dropItemNaturally(player.getLocation(), cleanStacks.remove(cleanStacks.size() - 1));
+        ItemHelper.dropItems(cleanStacks, inventory.getMaxStackSize(), player.getWorld(), player.getLocation());
         inventory.setStorageContents(cleanStacks.toArray(ItemStack[]::new));
+        player.updateInventory();
         return true;
     }
 
-    public static List<ItemStack> distinctItemStacks(final List<ItemStack> stacks){
+    public static List<ItemStack> distinctItemStacks(final List<ItemStack> stacks) {
         final List<ItemStack> distinctStacks = new ArrayList<>();
-        stacks.stream().filter(Objects::nonNull).forEach((final ItemStack stack) -> {
-            final Optional<ItemStack> similarStack = distinctStacks.stream()
-                    .filter(Objects::nonNull)
-                    .filter(theStack -> theStack.isSimilar(stack))
-                    .filter(theStack -> theStack.getAmount() < theStack.getMaxStackSize())
-                    .findFirst();
-
-            if(similarStack.isPresent()){
-                int totalAmount = stack.getAmount() + similarStack.get().getAmount();
-                while(totalAmount > stack.getMaxStackSize()){
-                    final ItemStack fullStack = stack.clone();
-                    fullStack.setAmount(stack.getMaxStackSize());
-
-                    distinctStacks.add(fullStack);
-                    totalAmount -= fullStack.getAmount();
-                }
-                similarStack.get().setAmount(totalAmount);
-            }else distinctStacks.add(stack.clone());
-        });
-
+        stacks.stream().filter(Objects::nonNull).forEach((final ItemStack stack) ->
+                distinctStacks.stream()
+                        .filter(Objects::nonNull)
+                        .filter(s -> s.getMaxStackSize() > 1)
+                        .filter(s -> s.getMaxStackSize() > s.getAmount())
+                        .filter(s -> s.isSimilar(stack))
+                        .findFirst()
+                        .ifPresentOrElse(similarStack -> {
+                            int maxSize = stack.getMaxStackSize();
+                            int totalAmount = stack.getAmount() + similarStack.getAmount();
+                            while (totalAmount > maxSize) {
+                                totalAmount -= maxSize;
+                                distinctStacks.add(ItemHelper.getStack(stack, maxSize));
+                            }
+                            similarStack.setAmount(totalAmount);
+                        }, () -> distinctStacks.add(stack.clone()))
+        );
         return distinctStacks;
     }
 
@@ -171,7 +151,7 @@ public class ChestOverflow extends JavaPlugin implements Listener, CommandExecut
             .thenComparing(ItemStack::getItemMeta, Comparator.nullsLast(META_COMPARATOR))
             .thenComparing(ItemStack::getAmount, Comparator.reverseOrder());
 
-    public static List<ItemStack> sortedItemStacks(final List<ItemStack> stacks){
+    public static List<ItemStack> sortedItemStacks(final List<ItemStack> stacks) {
         return stacks.stream().sorted(ChestOverflow.COMPARATOR).collect(Collectors.toList());
     }
 }
--- src/main/java/pe/chalk/bukkit/chestoverflow/ItemHelper.java
+++ src/main/java/pe/chalk/bukkit/chestoverflow/ItemHelper.java
@@ -1,7 +1,14 @@
 package pe.chalk.bukkit.chestoverflow;
 
 import com.google.common.collect.ImmutableMap;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -14,6 +21,21 @@ import java.util.Map;
  * @since 2023-01-16
  */
 public class ItemHelper {
+    public static boolean hasNormalInventory(final Block block) {
+        final BlockState state = block.getState();
+        if (!(state instanceof InventoryHolder holder)) return false;
+
+        final Inventory inventory = holder.getInventory();
+        return !(inventory instanceof FurnaceInventory || inventory instanceof BrewerInventory || inventory instanceof BeaconInventory);
+    }
+
+    public static Inventory getInventoryFromBlock(final Block block, final Player player) {
+        if (block == null) return null;
+        if (block.getType() == Material.ENDER_CHEST && player != null) return player.getEnderChest();
+        if (block.getState() instanceof InventoryHolder holder && ItemHelper.hasNormalInventory(block)) return holder.getInventory();
+        return null;
+    }
+
     public static Map<Enchantment, Integer> getStoredEnchants(ItemMeta meta) {
         return meta instanceof EnchantmentStorageMeta ? ((EnchantmentStorageMeta) meta).getStoredEnchants() : ImmutableMap.of();
     }
@@ -29,4 +51,16 @@ public class ItemHelper {
     public static List<String> getLore(ItemMeta meta) {
         return meta.hasLore() ? meta.getLore() : null;
     }
+
+    public static ItemStack getStack(ItemStack stack, int amount) {
+        final ItemStack s = stack.clone();
+        s.setAmount(amount);
+        return s;
+    }
+
+    public static void dropItems(final List<ItemStack> stacks, int maxSize, final World world, final Location location) {
+        while (stacks.size() > maxSize)
+            world.dropItemNaturally(location, stacks.remove(stacks.size() - 1));
+    }
+
 }
